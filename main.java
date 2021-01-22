ff28a6afc8b231808f71ad8e7e9b183544c268f6
leapbtw <36914991+leapbtw@users.noreply.github.com>
2021-01-22 11:15:31 +0100

    1.1
    windows 10 support on 1080p and 720p

--- main.java
+++ main.java
@@ -9,17 +9,54 @@ import java.util.*;
 import javax.swing.JOptionPane;
 
 public class main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		String OS = System.getProperty("os.name");
 		switch (OS) {
 			case "Mac OS X":
 				JOptionPane.showMessageDialog(null, "macOS machine detected");
+				(new mac(1280, 800)).start();
 				break;
+				
+				
 			case "Windows 10":
 				JOptionPane.showMessageDialog(null, "Windows 10 machine detected");
+				String[] choices = {"1920x1080", "1280x720", "custom [MAY NOT WORK WELL]"};
+				String input = (String) JOptionPane.showInputDialog(null, "please set a display size", "display size", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+				switch (input) {
+					case "1920x1080":
+						(new win(1920, 1080)).start();
+						break;
+					case "1280x720":
+						(new win(1280, 720)).start();
+						break;
+					default:
+						//custom screen resolution is supported, but the pixel checked is calculated with proportions, so even if it SHOULD work, it might not
+						//i suggest setting your display resolution to 1080p or to 720p instead of trying this, since i did tested those but i can't test your custom resolution
+						int customWinX = 0,customWinY = 0;
+						while (customWinX < 100) {
+						String input2 = JOptionPane.showInputDialog("input screen width");
+							try {
+								customWinX = Integer.parseInt(input2);
+							}
+							catch (NumberFormatException e)	{
+								JOptionPane.showMessageDialog(null, "input an integer please");
+								customWinX = 0;
+								break;
+							}
+						}
+						while (customWinY < 100) {
+						String input2 = JOptionPane.showInputDialog("input screen height");
+							try {
+								customWinY = Integer.parseInt(input2);
+							}
+							catch (NumberFormatException e)	{
+								JOptionPane.showMessageDialog(null, "input an integer please");
+								customWinY = 0;
+								break;
+							}
+						}
+				}
 				break;
 		}
-		(new mac()).start();
 	}
-	
 }
--- win.java
+++ win.java
@@ -4,9 +4,9 @@ import java.awt.event.KeyEvent;
 import java.util.*;
 import javax.swing.JOptionPane;
 
-public class mac extends Thread {
+public class win extends Thread {
 	int x, y;
-	mac(int x, int y) {
+	win(int x, int y) {
 	    this.x = x;
 	    this.y = y;
 	}
@@ -17,13 +17,18 @@ public class mac extends Thread {
 		try {
 			Robot r = new Robot();
 			//apre ALT TAB per far selezionare VBA-m
-			r.keyPress(KeyEvent.VK_META);
-			r.delay(20);
+			r.keyPress(KeyEvent.VK_ALT);
+			r.delay(50);
+			r.keyPress(KeyEvent.VK_CONTROL);
+			r.delay(50);
 			r.keyPress(KeyEvent.VK_TAB);
-			r.delay(10000);
-			r.keyRelease(KeyEvent.VK_META);
-			r.delay(20);
+			r.delay(50);
+			r.keyRelease(KeyEvent.VK_ALT);
+			r.delay(50);
+			r.keyRelease(KeyEvent.VK_CONTROL);
+			r.delay(50);
 			r.keyRelease(KeyEvent.VK_TAB);
+			r.delay(10000);
 			
 			while (isShiny(resets, x, y) == false) {
 				//softreset
@@ -33,10 +38,10 @@ public class mac extends Thread {
 				resets++;
 				//skip parte iniziale
 				for (int i = 0; i < 16; i++) {
-				    r.delay(200);
-				    r.keyPress(KeyEvent.VK_X);
-				    r.delay(50);
-				    r.keyRelease(KeyEvent.VK_X);
+					r.delay(200);
+					r.keyPress(KeyEvent.VK_X);
+					r.delay(50);
+					r.keyRelease(KeyEvent.VK_X);
 				}
 				//premo SU - inizio la scena di suicune
 				r.keyPress(KeyEvent.VK_W);
@@ -46,20 +51,26 @@ public class mac extends Thread {
 			}
 		}
 		catch (AWTException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 		JOptionPane.showMessageDialog(null, "SHINY FOUND after " + resets + " resets!");
 	}
 	
+	
 	public boolean isShiny (int resets, int x, int y) {
 	    int shinyX = 0, shinyY = 0;
 	    switch (y) {
-		case 800:
-		    if (x == 1280) {
-			shinyX = 890;
-			shinyY = 80;
+		case 1080:
+		    if (x == 1920) {
+			shinyX = 1270;
+			shinyY = 189;
 		    }
-			break;
+		    break;
+		case 720:
+			if (x == 1280) {
+				shinyX = 845;
+				shinyY = 113;
+			}
 		default:
 		    shinyX = (int) (x * 0.6953125);
 		    shinyY = (int) (y * 0.1);
@@ -75,9 +86,7 @@ public class mac extends Thread {
 		    e.printStackTrace();
 		}
 		System.out.println("SR: " + resets + " |\tcolor: " + suicuneColor);
-		if ("java.awt.Color[r=128,g=152,b=192]".equals(suicuneColor)) return true;
+		if ("java.awt.Color[r=128,g=152,b=192]".equals(suicuneColor) || "java.awt.Color[r=200,g=192,b=248]".equals(suicuneColor)) return true;
 		else return false;
 	}
 }
-
-//	    
